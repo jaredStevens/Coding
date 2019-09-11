a195536830d3f12f7a75015702766054355f1675
Jared Stevens <jared.b.stevens@gmail.com>
2019-09-11 16:17:18 -0500

    more practice

---
+++ algorithms.js
@@ -0,0 +1,27 @@
+/* stacks
+last in first out LIFO
+functions: push, pop, peek, length */
+
+//palindrone
+
+var letters = []; // this is our stack
+var word = "racecar"
+var rword = ""
+
+//put the letters of the word in our stack
+for (let i = 0; i < word.length; i++) {
+  letters.push(word[i])
+}
+
+//pop off the stack in reverse order
+for (let i = 0; i < word.length; i++) {
+  rword += letters.pop()
+}
+
+if(rword === word) {
+  console.log(word + " is a palindrone")
+}
+
+else {
+  console.log(word + " is not a palindrone")
+}
